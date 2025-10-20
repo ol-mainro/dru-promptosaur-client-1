f6a98b677286efb324a6009fabc209fe80e0cab0
olmainro <ol.mainro@gmail.com>
2025-10-20 18:49:08 +0200

    many changes

--- web/themes/custom/psaur_ai_blog/sw.js
+++
@@ -1,9 +0,0 @@
-// This code executes in its own worker or thread
-self.addEventListener("install", event => {
-  // console.log("Service worker installed");
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => {
-  // console.log("Service worker activated");
-  event.waitUntil(clients.claim());
-});
