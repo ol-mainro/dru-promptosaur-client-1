a8a391cbd2a6caa6ee0fd6452fe4194d2d3571e1
olmainro <ol.mainro@gmail.com>
2025-10-23 10:56:52 +0200

    many changes

--- ci/deploy-remote.sh
+++ ci/deploy-remote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-drush config:set system.maintenance message "Maintenance en cours..." -y
-drush state:set system.maintenance_mode 1 --input-format=integer
+vendor/bin/drush config:set system.maintenance message "Maintenance en cours..." -y
+vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer
 git fetch --all
 git clean -f
 git reset --hard HEAD
@@ -12,4 +12,4 @@ vendor/bin/drush cim -y
 # vendor/bin/drush content:import taxonomy_term --translate --assets --bundle="prompts_type" -y
 # vendor/bin/drush content:import taxonomy_term --translate --assets --bundle="prompts" -y
 vendor/bin/drush cr
-drush state:set system.maintenance_mode 0 --input-format=integer
+vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer
